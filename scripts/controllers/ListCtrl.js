6c3431888633c87765adf972cf988096a2d27f5c
Mauricio Bello <mow_bell@hotmail.com>
2016-04-12 12:40:40 -0500

    Markup home Book list

--- scripts/controllers/ListCtrl.js
+++ scripts/controllers/ListCtrl.js
@@ -1,10 +1,11 @@
 app.controller('ListCtrl', ['bookServices', function(bookServices) {
-    this.title = "HEY";
+    this.title = "MY WISH LIST";
     this.items = [];
     var that=this;
     bookServices.getItems()
     	.success(function(data, status, headers, config) {
     		that.items=data.buyTable;
+    		console.log(that.items);
         })
         .error(function(data, status, headers, config) {
 
